17ac2c99b05d8b4292aada9f369a509a27a8eac2
Sean <sean@organgrindermedia.com>
2022-11-06 06:25:20 -0500

    fix(list): multisig list is the authority items

--- src/lib/inq/listMultisigs.js
+++ src/lib/inq/listMultisigs.js
@@ -1,11 +1,12 @@
 import inquirer from "inquirer";
 
-export default (multisigs) => {
+export default (multisigs, defaultIndex) => {
     if(!multisigs || multisigs.length < 1){
         console.log("No multisigs found for this wallet");
     }
     const questions = [
         {
+        default: defaultIndex,
         type: 'list',
         name: 'action',
         message: 'Choose a multisig to manage',
